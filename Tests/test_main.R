d6ea2cb6ee0c3e5d29d7b55aa6bf5de3052d664d
J-Lucas <joylucas912@gmail.com>
2023-05-14 17:32:35 +0200

    added some test for init_board()

--- Tests/test_main.R
+++ Tests/test_main.R
@@ -7,9 +7,26 @@ library(testthat)
 # to avoid waringings when running tests
 Sys.setenv(LANG = "en_US.UTF-8")
 
+# set seed for random number generation
+set.seed(1)
+
 # test: init_player (n_real, n_computer)
 test_that("valid player", {
-  expect_true(init_player(2, 3) <= (2 + 3))
-  expect_true(init_player(7, 0) <= (7 + 0))
-  expect_true(init_player(1, 0) <= (1 + 0))
+    expect_true(init_player(2, 3) <= (2 + 3))
+    expect_true(init_player(7, 0) <= (7 + 0))
+    expect_true(init_player(1, 0) <= (1 + 0))
+})
+
+# test: init_board (n, n_row, n_col)
+test_that("correct board dimensions", {
+    board <- init_board(12, 3, 4)
+    expect_equal(dim(board), c(3, 4, 4))
+})
+
+test_that("correct initial values", {
+    board <- init_board(12, 3, 4)
+    expect_true(all(board[, , 1] >= 1 & board[, , 1] <= 13))
+    expect_true(all(board[, , 2] >= 1 & board[, , 2] <= 8))
+    expect_true(all(board[, , 3] == 0))
+    expect_true(all(board[, , 4] == 0))
 })
